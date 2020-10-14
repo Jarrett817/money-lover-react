e8e212a0e949c618da609d9cc24dee838cb0fb86
Jarrett <1206905682@qq.com>
2020-10-14 17:19:43 +0800

    默认tag

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,12 +1,15 @@
 import {useState} from "react";
 import {createId} from "./lib/createId";
 
+const defaultTags = [
+    {id: createId(), name: "抽烟"},
+    {id: createId(), name: "喝酒"},
+    {id: createId(), name: "烫头"},
+];
+
 const useTags = () => {
-    const [tags, setTags] = useState<{ id: number; name: string }[]>([
-            {id: createId(), name: "抽烟"},
-            {id: createId(), name: "喝酒"},
-            {id: createId(), name: "烫头"},
-        ]);
+    const [tags, setTags] = useState<{ id: number; name: string }[]>
+    (defaultTags);
     return {
         tags,
         setTags
