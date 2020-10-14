e3c6a74f128c2bf0f495ca7a5b2be44c4ae7ebf8
Jarrett <1206905682@qq.com>
2020-10-14 19:11:38 +0800

    抽离Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+type Props = {
+    label: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+const Input: React.FunctionComponent<Props> = (props) => {
+    const {label,children, ...rest} = props;
+    return (<Label>
+        <span>{props.label}</span>
+        <input {...rest}/>
+    </Label>);
+};
+
+const Label = styled.label`
+    display:flex;
+    align-items: center;
+    > span { margin-right: 16px; white-space: nowrap; }
+    > input {
+      display:block;
+      width: 100%;
+      height: 44px;
+      background: none;
+      border: none;
+    }
+`;
+
+export {Input};
+
+
+
